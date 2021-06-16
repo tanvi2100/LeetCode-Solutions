e1b0926d8fbd948c5cb206e5e7035bfa46bffb04
tanvi2100 <85941081+tanvi2100@users.noreply.github.com>
2021-06-16 16:30:59 +0530

    Remove Element

---
+++ 27.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeElement(vector<int>& a, int val) {
+        int n = a.size();
+        int j=0;
+        for(int i=0;i<n;i++){
+            if(a[i]!=val){
+                a[j++]=a[i];
+            }
+        }
+        return j;
+    }
+};
