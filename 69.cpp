0f98109f679e2a43e0dab6c32272458d2e8fb332
Tanvi <85941081+tanvi2100@users.noreply.github.com>
2021-06-22 16:34:08 +0530

    Sqrt(x)

---
+++ 69.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mySqrt(int n) {
+        if(n==0||n==1){
+            return n;
+        }
+        long long int start=1,end=n,ans;
+        while(start<=end){
+            long long int mid = (start+end)/2;
+            if((mid*mid) == n){
+                return mid;
+            }
+            else if((mid*mid) < n){
+                ans=mid;
+                start = mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return ans;
+    }
+};
