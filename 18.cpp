30a50ebd540186b4a73daaf06beeaa4d6f11f045
Tanvi Kshatriya <85941081+tanvi2100@users.noreply.github.com>
2021-07-17 07:15:59 +0530

    4Sum

--- 18.cpp
+++ 18.cpp
@@ -2,35 +2,31 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& a, int k) {
         int n = a.size();
-        if(n<4){
-            return {};
-        }
-          sort(a.begin(), a.end());
-        vector<vector<int>> results; 
-    for(int i=0;i<n-3;i++){
-         if(i == 0 || a[i] != a[i-1]) {
-        for(int j=i+1;j<n-2;j++){
-            int l=j+1;
-            int r = n-1;
-            while(l<r){
-                if(a[i]+a[j]+a[l]+a[r] == k){
-                    results.push_back({a[i], a[j], a[l], a[r]});
-                     while(l < r && a[l] == a[l+1]) l++;
-                    while(l < r && a[r] == a[r-1]) r--;
-                    l++;
-                    r--;
-                }
-                else if(a[i]+a[j]+a[l]+a[r] > k) {
-                    r--;
-                }
-                else{
-                    l++;
+        sort(a.begin(),a.end());
+        int left,right;
+        vector<vector<int>> ans;
+        for(int i=0;i<n-3;i++){
+            for(int j=i+1;j<n-2;j++){
+                left = j+1;
+                right = n-1;
+                while(left<right){
+                    int sum = a[i]+a[j]+a[left]+a[right];
+                    if(sum == k){
+                        ans.push_back({a[i],a[j],a[left],a[right]});
+                        left++;
+                        right--;
+                    }
+                    else if(sum>k){
+                        right--;
+                    }
+                    else{
+                        left++;
+                    }
                 }
             }
-             while(j < a.size()-2 && a[j] == a[j+1]) j++;
         }
-        }
-    }
-        return results;
+        sort(ans.begin(),ans.end());
+        ans.erase(unique(ans.begin(),ans.end()),ans.end());
+        return ans;
     }
 };
